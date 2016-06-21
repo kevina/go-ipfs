0cf8bb87f7f31a9dad332654e6bc28f6c79aa3c8
Lars Gierth <larsg@systemli.org>
2016-06-21 18:44:39 +0200

    WIP
    License: MIT
    Signed-off-by: Lars Gierth <larsg@systemli.org>

---
+++ core/coreapi/coreapi_test.go
@@ -0,0 +1,14 @@
+package coreapi_test
+
+import (
+	"testing"
+
+	"github.com/cheekybits/is"
+
+	coreapi "github.com/ipfs/go-ipfs/core/coreapi"
+	coreiface "github.com/ipfs/go-ipfs/core/coreapi/interface"
+)
+
+func TestImplementsInterface(t *testing.T) {
+
+}
